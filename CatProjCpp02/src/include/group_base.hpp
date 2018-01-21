c144da55ffe1d18ad1c7be2760d4ac506f8e927a
gmuel <gabmueller@hotmail.com>
2018-01-21 15:02:30 +0100

    Binary and terniary operation structure templates added Plus left and right hand composition of binary

--- CatProjCpp02/src/include/group_base.hpp
+++ CatProjCpp02/src/include/group_base.hpp
@@ -5,17 +5,56 @@ namespace alg {
 
 using namespace cat;
 
-template<typename T >
+template<typename T , typename BI_OPER>
 struct binary {
-	T operator()(const T& a, const T& b) const;
+	const BI_OPER& OPERATION;
 };
-template<typename T >
+template<typename T , typename TER_OPER>
 struct teriary {
+	const TER_OPER& OPERATION;
 	T operator()(const T& a, const T& b, const T& c) const ;
 };
-template<typename T >
-struct assoc : public eq_diag<teriary<T > > {
-	const 
+template<typename T , typename BINARY>
+struct left_hand : public teriary<T, left_hand<T, BINARY > > {
+	T operator()(const T& a, const T& b, const T& c) const {
+		return binary(binary(a,b),c);
+	}
+	left_hand(const BINARY& bin):binary(bin){}
+	left_hand(const left_hand<T, BINARY >& o):binary(o.binary){}
+	left_hand& operator=(const BINARY& bin){
+		binary = bin;
+
+		return *this;
+	}
+	~left_hand(){}
+protected:
+	BINARY binary;
+};
+template<typename T , typename BINARY>
+struct right_hand : public teriary<T, right_hand<T,BINARY > > {
+	T operator()(const T& a, const T& b, const T& c) const {
+		return binary(a,binary(b,c));
+	}
+	right_hand(const BINARY& bin):binary(bin){}
+	right_hand(const right_hand<T, BINARY >& o):binary(o.binary){}
+	right_hand& operator=(const BINARY& bin){
+		binary = bin;
+
+		return *this;
+	}
+	~right_hand(){}
+protected:
+
+	BINARY binary;
+};
+template<typename T , typename BINARY>
+struct assoc : public eq_diag<teriary<T, left_hand<T,BINARY > > > {
+	assoc(const BINARY& bin):operation(bin),left(bin),right(bin){}
+private:
+	const BINARY operation;
+	const left_hand<T,BINARY> 	left;
+	const right_hand<T,BINARY > right;
+
 };
 
 

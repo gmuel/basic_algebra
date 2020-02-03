7a603ade74d5179539bae017adc34ff50af07164
gmuel <gabmuelller@hotmail.com>
2020-02-03 22:06:44 +0100

    Initial build for mat testing

---
+++ CatProjCpp02/build/mat_test.mk
@@ -0,0 +1,20 @@
+#!/usr/bin/make -f
+BIN=$(PWD)bin
+SRC=$(PWD)src
+TAR=$(BIN)/mat_test
+LIS=$(SRC)/lib
+LIB=$(BIN)/lib
+CYC=$(BIN)/lib/cyclics.o
+MAI=$(SRC)/lib/mat_test.cpp
+CYS=$(SRC)/lib/cyclics.cpp
+CYH=$(SRC)/include/cyclics.hpp
+COM=$(BIN)/lib/mat_test.o
+CC=g++ -g
+$(TAR) : $(COM) $(CYC)
+	$(CC) $^ -o $@
+
+$(CYC) : $(CYS) $(CYH)
+	$(CC) -c $< -o $@ 
+	
+$(COM) : $(LIS)/mat_test.cpp $(SRC)/include/alg/1.1/algebra/algebra_matrix_base.hpp
+	$(CC) -c $< -o $@

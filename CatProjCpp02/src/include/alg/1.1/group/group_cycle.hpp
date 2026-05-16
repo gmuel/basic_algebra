cfb166007290fdb14c7a4ef4e31c04187d17a2fe
fun_gab <gabmueller@hotmail.com>
2026-05-16 14:52:41 +0200

    [c++/alg] improved cycle impl - new perm group started

---
+++ CatProjCpp02/src/include/alg/1.1/group/group_cycle.hpp
@@ -0,0 +1,222 @@
+/*
+ * cycle.hpp
+ *
+ *  Created on: 14.05.2026
+ *      Author: fun_gab
+ */
+
+#ifndef INCLUDE_CYCLE_HPP_
+#define INCLUDE_CYCLE_HPP_
+#include "/home/fun_gab/git/basic_algebra/CatProjCpp02/src/include/alg/1.1/group/group_group_base_impl.hpp"
+#include <utility>
+#include <map>
+namespace sym {
+
+template<unsigned int N >
+struct c_pair : public std::pair<alg::cyclic_wrp<N>, alg::cyclic_wrp<N> >{
+	typedef alg::cyclic_wrp<N> _cyc;
+	typedef std::pair<alg::cyclic_wrp<N>, alg::cyclic_wrp<N> > _base;
+	c_pair<N >* prev,* next;
+	c_pair(const _cyc& c1, const _cyc& c2):_base(c1,c2),prev(0),next(0){}
+	c_pair(_cyc&& c1, _cyc&& c2):_base(c1,c2),prev(0),next(0){}
+	c_pair(const _base& o):_base(o),prev(0),next(0){}
+	c_pair(_base&& o):     _base(o),prev(0),next(0){}
+	c_pair(explicit const c_pair<N>& o):_base(o),prev(0),next(o.next!=0?new c_pair<N> (*o.next):0){}
+	c_pair(explicit c_pair<N>&& o):     _base(o),prev(0),next(o.next){}
+	~c_pair(){
+		if(next!=0) {
+			delete next;
+			next = 0;
+		}
+		prev = 0;
+	}
+};
+
+template<unsigned int N >
+c_pair<N> make_cpair(const alg::cyclic_wrp<N>& c1, const alg::cyclic_wrp<N>& c2){
+	c_pair<N> cp(c1,c2);
+	return cp;
+}
+template<unsigned int N >
+c_pair<N> make_cpair(alg::cyclic_wrp<N>&& c1, alg::cyclic_wrp<N>&& c2){
+	c_pair<N> cp(c1,c2);
+	return cp;
+}
+
+template<unsigned int N >
+class cycle {
+public:
+	typedef alg::cyclic_wrp<N> 		_cyc;
+	typedef c_pair<N>				_cpa;
+	typedef std::map<_cyc,_cpa*>	_cma;
+	typedef typename _cma::const_iterator _c_it;
+	typedef typename _cma::iterator 		_it;
+	cycle(const _cyc& c1, const _cyc& c2):cpa(0),map(),first(0),last(0){
+		cpa = new _cpa(make_cpair<N>(c1, c2));
+		map[c1] = cpa;
+		last = first = cpa;
+		if(c1!=c2) {
+			cpa->next = new _cpa(make_cpair<N>(c2, c1));
+			cpa->next->prev = this;
+			last = map[c2] = cpa->next;
+		}
+	}
+	cycle(_cyc&& c1, _cyc&& c2):cpa(0),map(),first(0),last(0){
+		cpa = new _cpa(make_cpair<N>(c1, c2));
+		map[c1] = cpa;
+		last = first = cpa;
+		if(c1!=c2) {
+			create_pair(c2, c1, cpa);
+			last = map[c2] = cpa->next;
+		}
+	}
+	template<typename IT >
+	cycle(IT i, IT e):cpa(),map(),first(0),last(0){
+		insert(i,e);
+	}
+	~cycle(){
+		map.clear();
+		first = last = 0;
+		delete cpa;
+		cpa = 0;
+	}
+	iterator begin() {
+		iterator i = {cpa};
+		return i;
+	}
+	const_iterator cbegin() const {
+		const_iterator i = {cpa};
+		return i;
+	}
+	const_iterator cend() const {return const_iterator();}
+	iterator end() {return iterator();}
+	const_iterator find(const _cyc& c) const {
+		_c_it i = map.find(c);
+		return i==map.end()?cend():const_iterator{i->second};
+	}
+	iterator find(const _cyc& c) {
+		_it i = map.find(c);
+		return i==map.end()?end():iterator{i->second};
+	}
+	template<typename IT >
+	void insert(IT i, IT e){
+		if(last!=0){
+			iterator ii = {last};
+			return insert(i,e,ii);
+		}
+		if(i!=e){
+			_cyc frst = *i, lst = (i!=e)?*(++i):*i;
+			if(frst!=lst){
+				cpa = new _cpa(frst,lst);
+				map[frst] = lst;
+				create_pair(lst,frst, cpa);
+				insert(i,e,iterator{cpa});
+			}
+		}
+	}
+	/**
+	 * inserts elements returned by given iterator
+	 * iterator has to provide deref-operator returning
+	 * <code>alg::cyclic_wrp</code> to be used
+	 * @tparam IT iterator type - with defref op
+	 */
+	template<typename IT >
+	void insert(IT i, IT e, iterator hint){
+		if(hint==end()||i==e) return;
+		_cyc _frst = hint->first, _lst = hint->second;
+		_cpa* curr = hint.operator->(),* nxt = hint->next;
+		while(i!=e) {
+			_cyc snd = *i;
+			if(create_pair(_frst, snd, curr)){
+				curr = curr->next;
+				_frst = snd;
+			}
+			++i;
+		}
+		if(curr!=hint.operator ->()&&create_pair(_frst, _lst, curr)) {
+			curr = curr->next;
+			if(nxt!=0){
+				curr->next = nxt;
+				nxt->prev  = curr;
+			}
+			else last = curr;
+		}
+
+	}
+	friend struct const_iterator {
+		const _cpa* ptr = 0;
+		const_iterator& operator++() {
+			if(ptr!=0) ptr = ptr->next;
+			return *this;
+		}
+		const_iterator& operator--() {
+			if(ptr!=0) ptr = ptr->prev;
+			return *this;
+		}
+		const _cpa* operator->() const {
+			return ptr;
+		}
+		friend bool operator==(const const_iterator& i1, const const_iterator& i2){
+			return i1.ptr==i2.ptr;
+		}
+		friend bool operator!=(const const_iterator& i1, const const_iterator& i2){
+			return i1.ptr!=i2.ptr;
+		}
+	};
+	friend struct iterator {
+		_cpa* ptr = 0;
+		iterator& operator++() {
+			if(ptr!=0) ptr = ptr->next;
+			return *this;
+		}
+		iterator& operator--() {
+			if(ptr!=0) ptr = ptr->prev;
+			return *this;
+		}
+		_cpa* operator->() {
+			return ptr;
+		}
+		friend bool operator==(const iterator& i1, const iterator& i2){
+			return i1.ptr==i2.ptr;
+		}
+		friend bool operator!=(const iterator& i1, const iterator& i2){
+			return i1.ptr!=i2.ptr;
+		}
+	};
+	const _cyc& operator[](const _cyc& c) const {
+		const_iterator i = find(c);
+		return i!=cend()?i->second:c;
+	}
+	_cyc& operator[](_cyc& c) {
+		iterator i = find(c);
+		return i!=end()?i->second:c;
+	}
+private:
+	_cpa* cpa,* first,* last;
+	_cma map;
+	bool create_pair(const _cyc& _frst, const _cyc& _snd, _cpa* curr){
+		if(map.find(_snd)==map.end()){
+			auto* tmp = new _cpa(_frst,_snd);
+			map[_frst] = tmp;
+			tmp->prev = curr;
+			curr->next = tmp;
+			return true;
+		}
+		return false;
+	}
+	bool create_pair(_cyc&& _frst, _cyc&& _snd, _cpa* curr){
+		if(map.find(_snd)==map.end()){
+			auto* tmp = new _cpa(_frst,_snd);
+			map[_frst] = tmp;
+			tmp->prev = curr;
+			curr->next = tmp;
+			return true;
+		}
+		return false;
+	}
+};
+
+}
+
+
+#endif /* INCLUDE_CYCLE_HPP_ */
---
+++ CatProjCpp02/src/include/alg/1.1/group/group_sym.hpp
@@ -0,0 +1,137 @@
+/*
+ * sym.hpp
+ *
+ *  Created on: 12.05.2026
+ *      Author: fun_gab
+ */
+
+#ifndef INCLUDE_SYM_HPP_
+#define INCLUDE_SYM_HPP_
+#include "/home/fun_gab/git/basic_algebra/CatProjCpp02/src/include/alg/1.1/group/group_cycles.hpp"
+#include <set>
+#include <utility>
+namespace sym {
+
+using namespace alg;
+
+template<unsigned int N>
+std::set<cycle_element<N> > common_orbit(const cycle_element<N>& c1, const cycle_element<N>& c2){
+	std::set<cycle_element<N> > s;
+	bool lng = c1.length()<c2.length();
+	auto i = cycle_element<N>::const_cycle_iter(lng?c1:c2),
+			e = cycle_element<N>::const_cycle_iter(lng?c1:c2);
+	while(i!=e) {
+		if((lng && c2.contains(*i))||c1.contains(*i)) s.insert(*i);
+		++i;
+	}
+	return s;
+}
+
+template<unsigned int N>
+class sym {
+public:
+	typedef cyclic_wrp<N > 			_cyc;
+	typedef cycle_element<N > 		_cye;
+	typedef std::map<_cyc, _cye > 	_map;
+	sym(const _cyc& x1, const _cyc& x2):cyc_map(){
+		if(x1!=x2){
+			cyc_map[x1] = cyc_map[x2] = _cye(x1,x2);
+		}
+	}
+	sym(const _cye& c):cyc_map(){
+		this->add_cycle(c);
+	}
+	/**
+	 * Construct permutation of all
+	 * disjoint cycles returned by given iterator
+	 * @tparam IT iter type - deref operator must return a alg::cyclic_wrp&lt;N&gt;
+	 */
+	template<typename IT>
+	sym(IT i, IT e):cyc_map(){
+		for(; i!=e;++i) {
+			if(is_disjoint(*i))
+				add_cycle(*i);
+		}
+	}
+	sym(const sym& s):cyc_map(s.cyc_map){}
+	sym(sym&& s):cyc_map(s.cyc_map){}
+	~sym(){}
+	const _cyc& operator()(const _cyc& c) const {
+		auto i = cyc_map.find(c);
+		return i==cyc_map.end()?c:i->second(c);
+	}
+	sym& operator=(const _cye& c) {
+		cyc_map.clear();
+		add_cycle(c);
+		return *this;
+	}
+private:
+	_map cyc_map;
+	void add_cycle(const _cye& c) {
+		add_cycle(c,cyc_map);
+	}
+	void add_cycle(const _cye& c, _map& _mp) {
+			_cyc curr = c.firstElement();
+			do {
+				_mp[curr] = c;
+				curr = c(curr);
+			}while(curr!=c.firstElement());
+		}
+	bool is_disjoint(const _cye& c) const {
+		_cye* last(0);
+		for(auto i = cyc_map.begin(); i!=cyc_map.end();++i){
+			if(last==&(i->second)) continue;
+			last = &(i->second);
+			if(common_orbit(i->second,c).size()!=0) return false;
+		}
+		return true;
+	}
+	std::pair<_map,_map > joint_orbit(const sym<N>& s) const {
+		_map lhs, rhs;
+
+		bool lhs_sz = cyc_map.size()<s.cyc_map.size();
+		auto i = lhs_sz?cyc_map.begin():s.cyc_map.begin()
+				, e = lhs_sz?cyc_map.end():s.cyc_map.end();
+		while (i!=e){
+			auto ii = lhs_sz?s.cyc_map.find(i->first):cyc_map.find(i->first),
+					ee = lhs_sz?s.cyc_map.end():cyc_map.end();
+			if(ii!=ee) {
+				add_cycle( i->second, lhs);
+				add_cycle(ii->second, rhs);
+			}
+			++i;
+		}
+		return std::make_pair(lhs, rhs);
+	}
+	_map merge(const _cye& lhs, const _cye& rhs){
+		std::set<_cyc> orb = common_orbit(rhs, lhs);
+		_map comm;
+		_cye c;
+		for(auto i = orb.begin(); i!=orb.end();++i){
+
+		}
+		return comm;
+	}
+};
+template<>
+class sym<1u> {
+	sym(){}
+	sym(const sym<1u>& o){}
+	sym(sym<1u>&& o){}
+	sym<1u>& operator=(const sym<1u>& s){return *this;}
+public:
+	~sym(){}
+	static const sym<1u>& ID;
+	friend const sym<1u>& operator*(const sym<1u>& s1, const sym<1u>& s2) {return ID;}
+};
+
+template<>
+class sym<0u> : public sym<1u> {};
+
+template<>
+class sym<2u> : public cyclic_wrp<2u> {};
+
+}
+
+
+#endif /* INCLUDE_SYM_HPP_ */

2134e539076ffd211891e8a33a4116c566175db7
fun_gab <gabmueller@hotmail.com>
2026-05-21 16:50:48 +0200

    [c++/alg] reversal fct added and minor clean-up

--- CatProjCpp02/src/include/alg/1.1/group/group_cycle.hpp
+++ CatProjCpp02/src/include/alg/1.1/group/group_cycle.hpp
@@ -12,19 +12,26 @@
 #include <map>
 namespace sym {
 
+
+#define CYW_TYPE(N) \
+	typedef alg::cyclic_wrp<N>	_cyw##N
+
 template<unsigned int N >
 struct c_pair : public std::pair<alg::cyclic_wrp<N>, alg::cyclic_wrp<N> >{
-	typedef alg::cyclic_wrp<N> _cyc;
+	CYW_TYPE(N);
 	typedef std::pair<alg::cyclic_wrp<N>, alg::cyclic_wrp<N> > _base;
 	c_pair<N >* prev,* next;
-	c_pair(const _cyc& c1, const _cyc& c2):_base(c1,c2),prev(0),next(0){}
-	c_pair(_cyc&& c1, _cyc&& c2):_base(c1,c2),prev(0),next(0){}
+	c_pair(const _cywN& c1, const _cywN& c2):_base(c1,c2),prev(0),next(0){}
+	c_pair(_cywN&& c1, _cywN&& c2):_base(c1,c2),prev(0),next(0){}
 	c_pair(const _base& o):_base(o),prev(0),next(0){}
 	c_pair(_base&& o):     _base(o),prev(0),next(0){}
 	explicit c_pair(const c_pair<N>& o):_base(o),prev(0),next(0){
-		if(o.next!=0) next = new c_pair<N> (*o.next);
+		if(o.next!=0) {
+			next = new c_pair<N> (*o.next);
+			next->prev = this;
+		}
 	}
-	explicit c_pair(c_pair<N>&& o):     _base(o),prev(0),next(o.next){}
+	explicit c_pair(c_pair<N>&& o):     _base(o),prev(o.prev),next(o.next){}
 	~c_pair(){
 		if(next!=0) {
 			delete next;
@@ -38,13 +45,11 @@ struct c_pair : public std::pair<alg::cyclic_wrp<N>, alg::cyclic_wrp<N> >{
 
 template<unsigned int N >
 c_pair<N> make_cpair(const alg::cyclic_wrp<N>& c1, const alg::cyclic_wrp<N>& c2){
-	c_pair<N> cp(c1,c2);
-	return cp;
+	return c_pair<N>(c1,c2);
 }
 template<unsigned int N >
 c_pair<N> make_cpair(alg::cyclic_wrp<N>&& c1, alg::cyclic_wrp<N>&& c2){
-	c_pair<N> cp(c1,c2);
-	return cp;
+	return c_pair<N>(c1,c2);
 }
 template<unsigned int N >
 bool operator<(const alg::cyclic_wrp<N>& c1, const alg::cyclic_wrp<N>& c2){
@@ -139,8 +144,7 @@ public:
 			if(i!=map.end()) {
 				first = i->second;
 			}
-			auto ri = map.rbegin();
-			if(ri!=map.rend()) last = ri->second;
+			set_last();
 		}
 	}
 	cycle(cycle<N>&& o):cpa(o.cpa),map(o.map),first(o.first),_last(o._last){}
@@ -180,11 +184,11 @@ public:
 		iterator ii = last();
 		if(ii!=end()) return insert(i,e,ii);
 		if(i!=e){
-			_cyc frst = *i, lst = (i!=e)?*(++i):*i;
+			_cyc frst = *i;
+			_cyc lst = (++i!=e)?*i:frst;
 			if(frst!=lst){
 				cpa = new _cpa(frst,lst);
 				map[frst] = lst;
-				create_pair(lst,frst, cpa);
 				insert(i,e,iterator{cpa});
 			}
 		}
@@ -198,28 +202,53 @@ public:
 	template<typename IT >
 	void insert(IT i, IT e, iterator hint){
 		if(hint==end()||i==e) return;
-		_cpa* curr = hint.operator->(),* nxt = curr->next;
-		_cyc _frst = curr->first, _lst = curr->second;
+		_cpa* _crr = hint.operator->(),* _nxt = _crr->next;
+		_cyc _frst = _crr->first, _lst = _crr->second;
 		while(i!=e) {
-			_cyc snd = *i;
-			if(create_pair(_frst, snd, curr)){
-				curr = curr->next;
-				_frst = snd;
+			_cyc _snd = *i;
+			if(create_pair(_frst, _snd, _crr)){
+				_crr = _crr->next;
+				_frst = _snd;
 			}
 			++i;
 		}
-		if(curr!=hint.operator ->()&&create_pair(_frst, _lst, curr)) {
-			curr = curr->next;
-			if(nxt!=0){
-				curr->next = nxt;
-				nxt->prev  = curr;
+		if(_crr!=hint.operator ->()&&create_pair(_frst, _lst, _crr)) {
+			_crr = _crr->next;
+			if(_nxt!=0){
+				_crr->next = _nxt;
+				_nxt->prev  = _crr;
 			}
-			else _last = curr;
+			else _last = _crr;
 		}
-
+		set_last();
 	}
 	iterator last() {return iterator{_last};}
 	std::size_t length() const {std::size_t sz = map.size(); return sz==0?1:sz;}
+	cycle<N > reverse() const {
+		cycle<N>c;
+		auto i = clast(), e = cend();
+		std::size_t lng = length();
+		if(lng==1 || lng==2) return *this;
+		if(i!=e){
+			_cyc pre = first->first, im;
+			_cpa* curr (0);
+			while(i!=e) {
+				im = i->first;
+				if(c.cpa==0) {
+					c.cpa = new _cpa(pre,im);
+					curr = c.cpa;
+				}
+				else {
+					c.create_pair(pre, im, curr);
+					curr = curr->next;
+				}
+				pre = im;
+				--i;
+			}
+
+		}
+		return c;
+	}
 	const _cyc& operator[](const _cyc& c) const {
 		const_iterator i = find(c);
 		return i!=cend()?i.operator ->()->second:c;
@@ -245,6 +274,19 @@ public:
 	friend bool operator!=(const cycle<N>& c1, const cycle<N>& c2){
 		return !(c1==c2);
 	}
+	friend bool operator<(const cycle<N>& c1, const cycle<N>& c2){
+		std::size_t sz1 = c1.length(), sz2 = c2.length();
+		if(sz1!=sz2) return sz1<sz2?true:false;
+		auto i1 = c1.cbegin(), i2 = c2.cbegin(),
+				e1 = c1.cend(), e2 = c2.cend();
+		while(i1!=e1&&i2!=e2) {
+			if(i1->first<i2->first) return true;
+			if(i2->first<i1->first) return false;
+			++i1;
+			++i2;
+		}
+		return false;
+	}
 private:
 	_cma map;
 	_cpa* cpa,* first,* _last;
@@ -252,8 +294,13 @@ private:
 		if(map.find(_snd)==map.end()){
 			auto* tmp = new _cpa(_frst,_snd);
 			map[_frst] = tmp;
-			tmp->prev = curr;
-			curr->next = tmp;
+			if(curr!=0){
+				tmp->prev = curr;
+				curr->next = tmp;
+			}
+			else {
+				insertOrAppend(tmp);
+			}
 			return true;
 		}
 		return false;
@@ -262,21 +309,42 @@ private:
 		if(map.find(_snd)==map.end()){
 			auto* tmp = new _cpa(_frst,_snd);
 			map[_frst] = tmp;
-			tmp->prev = curr;
-			curr->next = tmp;
+			if(curr!=0){
+				tmp->prev = curr;
+				curr->next = tmp;
+			}
+			else {
+				insertOrAppend(tmp);
+			}
 			return true;
 		}
 		return false;
 	}
+	void insertOrAppend(_cpa* tmp){
+		if(cpa==0) {
+			cpa = tmp;
+		}
+		else if (_last!=0) {
+			_last->next = tmp;
+			tmp->prev   = _last;
+			_last = tmp;
+		}
+	}
+	void set_last(){
+		_cyc* curr = first;
+		if(curr==0) return;
+		while(curr->next!=0) {
+			if(curr->next->prev == 0) curr->next->prev = curr;
+			curr = curr->next;
+		}
+		if(_last!=curr) _last = curr;
+	}
 };
+#define CYC_TYPE(N) \
+	typedef cycle<N>		_cyc##N
 
 template<>
 class cycle<1u > {
-protected:
-	cycle(){}
-	cycle(const cycle<1u>& o){}
-	cycle(cycle<1u>&& o){}
-	cycle<1u>& operator=(const cycle<1u>& s){return *this;}
 public:
 	~cycle(){}
 	static const cycle<1u>& ID;
@@ -284,6 +352,12 @@ public:
 	friend bool operator==(const cycle<1u>& c1, const cycle<1u>& c2) {
 		return true;
 	}
+protected:
+	cycle(){}
+private:
+	cycle(const cycle<1u>& o){}
+	cycle(cycle<1u>&& o){}
+	cycle<1u>& operator=(const cycle<1u>& s){return *this;}
 };
 
 const cycle<1u>& cycle<1u>::ID = cycle();
@@ -292,10 +366,17 @@ class cycle<0u> : public cycle<1u > {
 public:
 	using cycle<1u>::ID;
 };
-/*template<>
-class cycle<2u> : public alg::cyclic_wrp<2u> {
-
-};*/
+template<>
+class cycle<2u> {
+public:
+	CYW_TYPE(2u);
+	CYC_TYPE(2u);
+	static const _cyw2u& ZERO;
+	static const _cyw2u& ONE;
+	const _cyw2u& operator[](const _cyw2u& c) const {
+		return c==ZERO?ONE:ZERO;
+	}
+};
 }
 
 
